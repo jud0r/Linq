80e0b1c18b928d11a6eb5ad27d467960597f6245
Thiago Silva <thiago.silva@outlook.com>
2020-05-16 08:41:27 -0300

    LINQ com lambda parte 1

---
+++ IntroLinq/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace IntroLinq.Entities
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Price.ToString("F2")
+                + ", "
+                + Category.Name
+                + ", "
+                + Category.Tier;
+        }
+    }
+}
